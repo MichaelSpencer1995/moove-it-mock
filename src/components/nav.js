032c838c68c682e7ce6be5dc06853b10e599b026
michael.spencer1995 <michael.spencer1@g.austincc.edu>
2018-07-07 14:37:02 -0500

    added assets, nav styles added

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,7 +7,7 @@ class Nav extends Component {
             <View>
                 <Container>
                     <Logo>
-                        <img src="#" />
+                        <LogoSvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 35" class="u-fill--white u-position--absolute u-is-centered--total" width="30px"><path d="M4.7 3.6c-.2 0-.5.1-.8.2l-3.1 1c-.4.2-.6.6-.6 1.1v24.7c0 .6.1.8.8.8h5.4c.6 0 .8-.2.8-.8v-19c.7-.7 2.5-2.2 4.8-2.2 2.1 0 2.3 2.5 2.3 4.5v16.7c0 .6.1.8.8.8h5.4c.6 0 .8-.2.8-.8v-19c.7-.7 2.5-2.2 4.8-2.2 2.1 0 2.3 2.5 2.3 4.5v16.7c0 .6.1.8.8.8H34c.6 0 .8-.2.8-.8V12.3c0-4.7-1.2-8.6-6.3-8.6-4.4 0-6.8 2.3-8.4 3.7-.7-2.3-3.2-3.7-5.5-3.7-4.3 0-6.7 2.3-8.3 3.7C5.6 5 5.2 3.8 4.7 3.6z"></path></LogoSvg>
                     </Logo>
 
                     <Links>
@@ -29,13 +29,68 @@ class Nav extends Component {
     }
 }
 
-const View = styled.div``
-const Container = styled.div``
-const Logo = styled.div``
-const Links = styled.ul``
-const Link = styled.li``
+const View = styled.div`
+    position: fixed;
+    width: 100%;
+    height: 70px;
+    background: transparent;
+    background: #ccc;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 10px;
+    box-sizing: border-box;
+`
+const Container = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+const Logo = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #ff8d40;
+    height: 50px;
+    width: 50px;
+    border-radius: 5px;
+`
+const LogoSvg = styled.svg`
+    fill: white;
+`
+const Links = styled.ul`
+    display: flex;
+    text-align: center;
+    list-style: none;
+    align-self: center;
+`
+const Link = styled.li`
+    margin-right: 20px;
+    
+    a {
+        font-size: 14px;
+        font-weight: 100;
+        text-decoration: none;
+        color: white;
+    }
+`
 const ContactInfo = styled.div``
-const PhoneNumber = styled.a``
-const ContactButton = styled.button``
+const PhoneNumber = styled.a`
+    color: #ff8d40;
+    font-size: 20px;
+    font-weight: 300;
+`
+const ContactButton = styled.button`
+    margin-left: 15px;
+    margin-right: 15px;
+    border: #fff 1px solid;
+    border-radius: 999px;
+    padding: 10px 20px;
+    color: white;
+    font-size: 14px;
+    font-weight: 100;
+`
 
 export default Nav
