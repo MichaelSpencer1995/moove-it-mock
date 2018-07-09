840c017cf2e6fdd7b115244e5f4181316117dda9
michael.spencer1995 <michael.spencer1@g.austincc.edu>
2018-07-09 13:02:36 -0500

    restyled nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,9 +6,11 @@ class Nav extends Component {
         return (
             <View>
                 <Container>
-                    <Logo>
-                        <LogoSvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 35" width="30px"><path d="M4.7 3.6c-.2 0-.5.1-.8.2l-3.1 1c-.4.2-.6.6-.6 1.1v24.7c0 .6.1.8.8.8h5.4c.6 0 .8-.2.8-.8v-19c.7-.7 2.5-2.2 4.8-2.2 2.1 0 2.3 2.5 2.3 4.5v16.7c0 .6.1.8.8.8h5.4c.6 0 .8-.2.8-.8v-19c.7-.7 2.5-2.2 4.8-2.2 2.1 0 2.3 2.5 2.3 4.5v16.7c0 .6.1.8.8.8H34c.6 0 .8-.2.8-.8V12.3c0-4.7-1.2-8.6-6.3-8.6-4.4 0-6.8 2.3-8.4 3.7-.7-2.3-3.2-3.7-5.5-3.7-4.3 0-6.7 2.3-8.3 3.7C5.6 5 5.2 3.8 4.7 3.6z"></path></LogoSvg>
-                    </Logo>
+                    <LogoContainer>
+                        <Logo>
+                            <LogoSvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 35" width="30px"><path d="M4.7 3.6c-.2 0-.5.1-.8.2l-3.1 1c-.4.2-.6.6-.6 1.1v24.7c0 .6.1.8.8.8h5.4c.6 0 .8-.2.8-.8v-19c.7-.7 2.5-2.2 4.8-2.2 2.1 0 2.3 2.5 2.3 4.5v16.7c0 .6.1.8.8.8h5.4c.6 0 .8-.2.8-.8v-19c.7-.7 2.5-2.2 4.8-2.2 2.1 0 2.3 2.5 2.3 4.5v16.7c0 .6.1.8.8.8H34c.6 0 .8-.2.8-.8V12.3c0-4.7-1.2-8.6-6.3-8.6-4.4 0-6.8 2.3-8.4 3.7-.7-2.3-3.2-3.7-5.5-3.7-4.3 0-6.7 2.3-8.3 3.7C5.6 5 5.2 3.8 4.7 3.6z"></path></LogoSvg>
+                        </Logo>
+                    </LogoContainer>
 
                     <Links>
                         <Link><a href="#">Home</a></Link>
@@ -46,7 +48,11 @@ const Container = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    > * {
+        flex-basis: 33.33333%;
+    }
 `
+const LogoContainer = styled.div``
 const Logo = styled.div`
     display: flex;
     justify-content: center;
@@ -75,7 +81,11 @@ const Link = styled.li`
         color: white;
     }
 `
-const ContactInfo = styled.div``
+const ContactInfo = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`
 const PhoneNumber = styled.a`
     color: #ff8d40;
     font-size: 20px;
