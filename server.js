8a0b681f9546766e5310386801d1027de6b5e04a
michael.spencer1995 <michael.spencer1@g.austincc.edu>
2018-07-12 15:47:00 -0500

    added server, pre-build

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+app.use(express.static(path.resolve(__dirname, 'build')))
+
+app.get('/', (req, res) => {
+    res.sendfile(path.resolve(__dirname, 'build', 'index.html'))
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log('connected to port', port)
+})
